d778bde8d4ab804e32348bea355b1015330cff12
= <=>
2024-01-09 23:45:38 -0300

    Creating Buttons

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -15,26 +15,84 @@ class SignInScreen extends StatelessWidget {
               color: Colors.green[300],
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  CustomTextField(
-                    icon: Icons.email_rounded,
-                    label: 'Email',
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
+              color: Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const CustomTextField(
+                  icon: Icons.email_rounded,
+                  label: 'Email',
+                ),
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    )),
+                    onPressed: () {},
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                  CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isSecret: true,
-                  )
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
+                      )),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(width: 2, color: Colors.green),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Criar conta', style: TextStyle(fontSize: 18),),
+                  ),
+                ),
+              ],
             ),
           )
         ],
